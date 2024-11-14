b81ade7574b0b11a43b3e20ea41381df7185ceb9
Aditya Kumar Saini <adityasainics10@gmail.com>
2024-11-14 23:56:56 +0530

    route to create outbox and main table

--- hooks/src/index.ts
+++ hooks/src/index.ts
@@ -1,13 +1,38 @@
 import express from 'express'
+import {PrismaClient} from "@prisma/client"
 
+const client = new PrismaClient();
 const app = express();
+app.use(express.json())
 
-app.post('/hooks/catch/:userId/:zapId',(req,res)=>{
+app.post('/hooks/catch/:userId/:zapId',async (req,res)=>{
     const userId = req.params.userId;
     const zapId = req.params.zapId;
+    const body = req.body;
 
-    // store in db a new trigger 
 
-    // push it to a queue (kafka/redis)
+    // store in db a new trigger
+    // use transacition  so that data enter in both table or
+    // not in any table 
+    await client.$transaction(async tx => {
+        const run = await tx.zapRun.create({
+            data: {
+                zapId: zapId,
+                metadata:body
+            }
+        });;
+
+        await tx.zapRunOutbox.create({
+            data: {
+                zapRunId: run.id
+            }
+        })
+    })
+
+    res.json({
+        meassage:"Webhook recieved"
+    })
 
 })
+
+app.listen(3000);
