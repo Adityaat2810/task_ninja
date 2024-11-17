dfbef3fde64a0b06b521a2e0915060c9790ebfb6
Aditya Kumar Saini <adityasainics10@gmail.com>
2024-11-17 11:31:41 +0530

    bootstraped processor

--- processor/src/index.ts
+++ processor/src/index.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from "@prisma/client"
 import { Kafka } from "kafkajs"
 const client = new PrismaClient();
 
-const TOPIC_NAME= 'zap-events-events'
+const TOPIC_NAME= 'zap-events'
 const kafka = new Kafka({
   clientId:"outbox-processor",
   brokers:['localhost:9092']
